6c39dc05ce4cb4c36a20486c7fed487f86427c32
Thiago Ferauche <thiago.ferauche@gmail.com>
2025-10-01 21:02:04 -0300

    ...

--- exemplo_pytest/calculadora.py
+++ exemplo_pytest/calculadora.py
@@ -5,3 +5,8 @@ def somar(a, b):
 def subtrair(a, b):
     """Retorna a diferença entre dois números."""
     return a - b
+
+"""
+O pytest automaticamente encontra testes em arquivos que começam ou terminam com o 
+prefixo test_. As funções de teste também devem começar com test_.
+"""
